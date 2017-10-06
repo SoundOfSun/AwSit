685591dfc0f464af32f4bba53032f535cda45a5a
ralph kemp <ralphk3mp@gmail.com>
2017-10-06 12:25:20 +0100

    created initial database and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006103815) do
+ActiveRecord::Schema.define(version: 20171006112254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.text "message"
+    t.string "status"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "recommendations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "booking_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_recommendations_on_booking_id"
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "type"
@@ -44,5 +63,8 @@ ActiveRecord::Schema.define(version: 20171006103815) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "recommendations", "bookings"
+  add_foreign_key "recommendations", "users"
   add_foreign_key "skills", "users"
 end
