8d905c185b7e07531f49a246ad4381db06981479
ralph kemp <ralphk3mp@gmail.com>
2017-10-06 14:09:43 +0100

    added to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to dashboard_path
   end
 
   private
