2eb5dbfbe79f85297d447b230371bc8270762516
Cecile Alfred <cecile.alfredx@gmail.com>
2017-10-21 22:29:58 +0100

    Modified the seed to test further the logic on dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    require.params(:user).permit(:name, :address)
+    require.params(:user).permit(:name, :babysitter, :address)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ User.destroy_all
 p "Seeding Users..."
 
 u1 = User.create!(email: 'parent@awsit.com', password: '123456', name: 'Beth Sanchez', babysitter: false, address: '55 usher road, London E3 2HA')
-u2 = User.create!(email: 'babysitter@awsit.com', password: '123456', name: 'Rick Sanchez', babysitter: true, address: '101 Christan St, London E1 1RX')
+u2 = User.create!(email: 'babysitter@awsit.com', password: '123456', name: 'Rick Sanchez', babysitter: true, address: '101 Christan St, London E1 1RX', profile_description: 'Hi, I\'m Super Rick ! I rock, I\'m the best and I took care of many a Morty in my time. Not like that *sshole Jerry. Just trust me, I know what kids love.')
 
 p "Users have successfully been created !"
