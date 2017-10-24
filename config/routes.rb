0fd8a926617dd63e8716cfd57379eb49493ad8db
ralph kemp <ralphk3mp@gmail.com>
2017-10-24 02:10:42 +0100

    goingt o bed but still can't fucking figure out devise ajax what the fuck

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'dashboard', to: 'users#dashboard'
 
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions: 'users/sessions'
+}
   resources :skills, only: [:new]
   resources :users, only: [:edit, :show, :index]
 end
