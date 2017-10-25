883e83e7b2e11558e8aeca5b312d0be5b15014e2
Cecile Alfred <cecile.alfredx@gmail.com>
2017-10-25 18:36:35 +0100

    created some files to prepare the remote true form

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,4 @@
 class SkillsController < ApplicationController
-
   def index
     @skills = Skill.all
   end
@@ -10,13 +9,39 @@ class SkillsController < ApplicationController
   end
 
   def create
+    # Create a new instance of Skill
     @skill = Skill.new(skill_params)
+      if @skill.save
+        respond_to do |format|
+          format.html { redirect_to new_skill_path(@skill) }
+          format.js # <-- will render `app/views/skills/create.js.erb`
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to new_skill_path(@skill) }
+          format.js # <-- idem
+        end
+      end
+    # Set the Skill's user to current_user
     @skill.user = current_user
+    # Save the skill
     @skill.save
+    # Pundit
     authorize @skill
   end
 
   def destroy
+    # Find a specific skill
+    @skill = Skill.find(params[:id])
+    # Destroy it
+    @skill.destroy
+    # Redirect to the new_skill (to add or remove)
+    redirect_to new_skill_path(@skill)
   end
 
+  private
+
+  def skill_params
+    params.require(:skill).permit(:content)
+  end
 end
